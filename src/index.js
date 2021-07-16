ce0752eb49720837c0462016c0106a3726d79171
John Pertuz <Johnpertuz@gmail.com>
2021-07-16 10:07:02 -0500

    Configurando parcel y typeScript para pasar nuestro proyecto a Ts

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ buttonMute.onclick = () => player.toogleMuteUnMute();
 
 
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').catch(error=> {
+    navigator.serviceWorker.register('../sw.js').catch(error=> {
         console.log(error.message);
     });
 }
